9da9c5b39ed52639c7aca25c5a4c4c5bb7027aed
Steven Mitchell <smitch15@binghamton.edu>
2017-12-09 11:04:20 -0500

    update final project 2

--- AStarWithMotors/AStarWithMotors.ino
+++ AStarWithMotors/AStarWithMotors.ino
@@ -1,4 +1,5 @@
-#define OBSTACLE_SIZE 3
+
+#define OBSTACLE_SIZE 1
 
 class Node{
  public:
@@ -8,17 +9,153 @@ class Node{
   byte infoBits = 0;
 };
 
+Node graph[16][16];
+
+const PROGMEM uint8_t srcXandY[] = {0,0};
+const PROGMEM uint8_t destXandY[] = {10,15};
+
+int inPath[255];
+
 
 void setup() {
   Serial.begin(9600);
+
+  // put your setup code here, to run once:
+  for (int i = 0; i<16; i++){
+    for (int j = 0; j < 16; j++){
+      graph[i][j].infoBits = 0;
+    }
+  }
+  int num = 0;
+  for (int i = 0; i<16; i++){
+     for (int j = 0; j < 16; j++){
+        if (graph[i][j].infoBits &1000){
+          num++;
+        }
+      }
+   }
+  
+  Serial.println("INIT");
+  Serial.print(num); Serial.println(" nodes in path");
+  aStar();
+
+  Serial.println("AFTER A STAR");
+  Serial.println("in path coords.");
+  for (int k = 0; k < 255; k++){
+    Serial.print(inPath[k]);
+    if (k == 255) break;
+    Serial.print(", ");
+  }
+  Serial.println();
+  byte initDir = 1;
+  for (int i = inPath[0]*2-1; i > 0; i-=2){
+    // x's 
+    if (inPath[i] - inPath[i-2] != 0){ // if the x value is off
+      if (inPath[i] > inPath[i-2]){ // if it needs to turn left or right
+        // go left
+        if (initDir != 3){
+          switch(initDir){
+            case 0:
+              turnLeft();
+              break;
+            case 1:
+              doA180();
+              break;
+            case 2:
+              turnRight();
+              break;
+            default:
+              break;
+          }
+        }
+        initDir = 3;
+       }
+        
+        if (inPath[i] < inPath[i-2]){ // if it needs to turn left or right
+          // go right
+          if (initDir != 1){
+            switch(initDir){
+              case 0:
+                turnRight();
+                break;
+              case 3:
+                doA180();
+                break;
+              case 2:
+                turnLeft();
+                break;
+              default:
+                break;
+            }
+          }
+          initDir = 1;
+        }
+        
+     // POINT OF POTENTIAL TOM FOOLERY --> check format of array
+    // y's
+    } else {    // the y values are off
+      if (inPath[i+1] > inPath[i-1]){ // if it needs to turn up or down
+        // go up
+        if (initDir != 0){
+          switch(initDir){
+            case 3:
+              turnRight();
+              break;
+            case 1:
+              turnLeft();
+              break;
+            case 2:
+              doA180();
+              break;
+            default:
+              break;
+          }
+        }
+        initDir = 0;
+       }
+        
+        if (inPath[i+1] < inPath[i-1]){ // if it needs to turn left or right
+          // go right
+          if (initDir != 2){
+            switch(initDir){
+              case 0:
+                doA180();
+                break;
+              case 3:
+                turnLeft();
+                break;
+              case 1:
+                turnRight();
+                break;
+              default:
+                break;
+            }
+          }
+          initDir = 2;
+        }
+    }
+
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+void aStar() {
+//  Serial.begin(9600);
 //  Serial.print('a');
   // obstacle X's and Y's 
-  const PROGMEM uint8_t obstacleX[] = {};
-  const PROGMEM uint8_t obstacleY[] = {};
-  const PROGMEM uint8_t srcXandY[] = {0,0};
-  const PROGMEM uint8_t destXandY[] = {0,15};
+  const PROGMEM uint8_t obstacleX[] = {0};
+  const PROGMEM uint8_t obstacleY[] = {5};
 
-  Node graph[16][16];
+
+//  Node graph[16][16];
+ 
 //  Serial.print('b');
   
   // set all g and h costs of each node in graph to infinity(255)
@@ -34,18 +171,17 @@ void setup() {
   graph[srcXandY[0]][srcXandY[1]].gCost = 0;
   
   // set heuristic value of start node
-  graph[srcXandY[0]][srcXandY[1]].hCost = abs((destXandY[0] - srcXandY[0]))
-                                          + abs((destXandY[1] - srcXandY[1]));
-                                          
+  graph[srcXandY[0]][srcXandY[1]].hCost = abs((destXandY[0] - srcXandY[0])) + abs((destXandY[1] - srcXandY[1]));
+
 //  Serial.println(graph[srcXandY[0]][srcXandY[1]].hCost);
 //  while(1){}
   // put the startnode in the queue, set the source node bit
   graph[srcXandY[0]][srcXandY[1]].infoBits |= 0b10001010; 
     
   // set the info bit for known node obstacles
-//  for (byte i = 0; i < OBSTACLE_SIZE; i++){
-//    (graph[obstacleX[i]][obstacleY[i]]).infoBits |= 0b100;
-//  }
+  for (byte i = 0; i < OBSTACLE_SIZE; i++){
+    (graph[obstacleX[i]][obstacleY[i]]).infoBits |= 0b100;
+  }
 
   // set the destination node bits
   graph[destXandY[0]][destXandY[1]].infoBits |= 0b1001;
@@ -91,15 +227,15 @@ void setup() {
         }
       }
     }
-    Serial.println();
-    Serial.print("xMin: ");
-    Serial.println(xMin);
-    
-    
-    Serial.print("yMin: ");
-    Serial.println(yMin);
-    Serial.println();
-    
+//    Serial.println();
+//    Serial.print("xMin: ");
+//    Serial.println(xMin);
+//    
+//    
+//    Serial.print("yMin: ");
+//    Serial.println(yMin);
+//    Serial.println();
+//    
     if (current->infoBits & 0b1){ // if current is destination
 //      Serial.println(srcXandY[0] << 4);
 //      Serial.println(srcXandY[1]);
@@ -107,41 +243,59 @@ void setup() {
 //      byte num = 0;
 //      current->infoBits |= 0b1000; // add to path
       Serial.println();
+
+      Serial.println();
       Serial.println("Found Destination Node");
-      Serial.print("Current parentXY = ");
-      Serial.println(current->parentXY);
-      Serial.print("Source XY = ");
-      Serial.println(sourceXY);
+//      Serial.print("Source XY = ");
+//      Serial.println(sourceXY);
       Serial.println(); 
+      byte count = 0;
+      byte index = 3; 
+      inPath[1] = destXandY[0];
+      inPath[2] = destXandY[1];
       while (current->parentXY != sourceXY){
-//        Serial.print('f');
-//        Serial.println("curent's parent: ");
-//        Serial.println(current->parentXY);
-//        Serial.println("source xy: ");
-//        Serial.println(sourceXY);
-//        Serial.println("reconstruct");
         current->infoBits |= 0b1000; // add to path
+        count++;
+        inPath[index] = current->parentXY >> 4;
+        index++;
+        inPath[index] = current->parentXY &0b1111;
+        index++;
         current = &(graph[current->parentXY >> 4][current->parentXY & 0b1111]);
-//        Serial.println("node num "); Serial.println(num);
-//        Serial.println("path x, y"); Serial.println(current->parentXY >> 4); 
-//        Serial.println(current->parentXY & 0b1111);
-        Serial.println();
-        Serial.println("Found Destination Node");
-        Serial.print("Current parentXY = ");
-        Serial.println(current->parentXY);
-        Serial.print("Source XY = ");
-        Serial.println(sourceXY);
+        if (current->infoBits & 0b1000){
+          Serial.println("ADDED TO PATH");
+        }
+        int num = 0;
+        for (int i = 0; i<16; i++){
+          for (int j = 0; j < 16; j++){
+            if (graph[i][j].infoBits &1000){
+              num++;
+            }
+          }
+        }
+        Serial.print(num); Serial.println(" nodes in path");
+        Serial.print("Current parent X, Y = ");
+
+        Serial.println(current->parentXY >> 4);
+        Serial.println(current->parentXY & 0b1111);
+        Serial.print("Source X, Y= ");
+        Serial.println(srcXandY[0]);
+        Serial.println(srcXandY[1]);
         Serial.println(); 
         
       }
+      inPath[0] = count+1;
+      inPath[count*2+3] = srcXandY[0];
+      inPath[count*2+4] = srcXandY[1];
+      Serial.print(count); Serial.println(" ADDED TO PATH");
      
       Serial.println("SUCCESS");
-      while(1)  continue;
+      return;
     }
     
     // remove min score from queue
     current->infoBits &= 0b01111111;  //remove from queue bit
     current->infoBits |= 0b01000000;  //add to visited 
+    
 ////////// setup for loop //////////////////////
     // check if neighbor is new, then add it to the open queue 
     // not in the queue and not been visited
@@ -175,9 +329,10 @@ void setup() {
     byte dist, h_offset;
     for (int i=0;i<4;i++){
       if (nebArr[i] == 0)  continue;  // no neighbor
+      if (nebArr[i]->infoBits & 0b100)  continue; // is an obstacle
       if (nebArr[i]->infoBits & 0b01000000) continue; // in closed set
       if (!(nebArr[i]->infoBits & 0b10000000)) 
-        nebArr[i]->infoBits |= 0b10001010; // add to queue
+        nebArr[i]->infoBits |= 0b10000000; // add to queue
       
 //      Serial.print("d----");
 //      Serial.println(current->parentXY);
@@ -209,6 +364,7 @@ void setup() {
       
       nebArr[i]->gCost = tent_cost;
       nebArr[i]->hCost = abs(destXandY[0] - xMin) + abs(destXandY[1] - yMin) -1;
+//      Serial.println("yo");
      
     }
 
@@ -220,12 +376,8 @@ void setup() {
         } 
       }
     }
+    
   }
 }
 
-
-
-void loop(){
-  
-}
 
